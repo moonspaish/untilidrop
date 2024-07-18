64c181567c455194e8fb470e13432c4417f3c54a
moonspaish <69521713+moonspaish@users.noreply.github.com>
2024-07-18 18:37:54 +0300

    added a pip upgrade and a check to see if they have python

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,15 @@
-python -m venv venv
+if ! command -v python3.9 &> /dev/null
+then
+    echo "Python3.9 could not be found"
+    exit
+fi
+
+# Create and activate virtual environment
+python3.9 -m venv venv
 source venv/bin/activate
+
+# Upgrade pip to ensure we have the latest version
+pip install --upgrade pip
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
 python3.9 manage.py migrate
