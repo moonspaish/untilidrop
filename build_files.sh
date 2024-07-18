63449b5fca3d994fea1a5b4b0695f8a61740dbc7
moonspaish <69521713+moonspaish@users.noreply.github.com>
2024-07-18 19:16:17 +0300

    justpython

--- build_files.sh
+++ build_files.sh
@@ -1,17 +1,6 @@
-#!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# Check if python3.9 is installed
-if ! command -v python3.9 &> /dev/null
-then
-    echo "Python3.9 could not be found"
-    exit 1
-fi
 
 # Create and activate virtual environment
-python3.10 -m venv venv
+python -m venv venv
 source venv/bin/activate
 
 # Upgrade pip to ensure we have the latest version
@@ -20,5 +9,5 @@ source venv/bin/activate
 pip install -r requirements.txt --verbose
 
 # Run Django commands
-python3.10 manage.py collectstatic --noinput
-python3.10 manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py migrate
