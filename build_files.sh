db1f6aeabff7b09840e965eb1d85ea91936fb9db
moonspaish <69521713+moonspaish@users.noreply.github.com>
2024-07-18 19:21:27 +0300

    holy shit

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,8 @@
 
-# Create and activate virtual environment
-python3.9 -m venv venv
-source venv/bin/activate
-
-# Upgrade pip to ensure we have the latest version
-
 # Install dependencies with increased verbosity
-pip install -r requirements.txt --verbose
+python3.9 -m pip install -r requirements.txt --verbose
 
-# Run Django commands
-python3.9 manage.py collectstatic --noinput
-python3.9 manage.py migrate
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+python3.9 manage.py collectstatic --noinput --clear
+
